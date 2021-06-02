d132a61557a66eb731b053b41f791f9ff6d1208c
takuro.katsube <takuroktb@gmail.com>
2021-06-02 14:28:28 +0000

    kadai

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,17 @@
+def fizzBuzz(num)
+        if num % 3 == 0  && num % 5 == 0
+            return  "FizzBuzz"
+        elsif num % 5 == 0
+            return  "Buzz"
+        elsif num % 3 == 0
+            return  "Fizz"
+        else 
+            return num
+        end
+        
+end
+num_max = 100
+
+(1..num_max).each do |number|
+    puts fizzBuzz(number)
+end
