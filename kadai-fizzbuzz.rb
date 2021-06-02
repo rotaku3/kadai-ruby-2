ff745f43ca460485ffc9c97fce7d9cfff38c055a
takuro.katsube <takuroktb@gmail.com>
2021-06-02 14:36:52 +0000

    first commit

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,13 +1,13 @@
 def fizzBuzz(num)
-        if num % 3 == 0  && num % 5 == 0
-            return  "FizzBuzz"
-        elsif num % 5 == 0
-            return  "Buzz"
-        elsif num % 3 == 0
-            return  "Fizz"
-        else 
-            return num
-        end
+    if num % 3 == 0  && num % 5 == 0
+        return  "FizzBuzz"
+    elsif num % 5 == 0
+        return  "Buzz"
+    elsif num % 3 == 0
+        return  "Fizz"
+    else 
+        return num
+    end
         
 end
 num_max = 100
